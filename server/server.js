7c8d87f9ccb76e45feb9341d72290470527b858e
Jose Mª R.F <Jose M R.F>
2017-02-17 09:31:45 +0100

    port

--- server/server.js
+++ server/server.js
@@ -4,8 +4,10 @@ var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
+/* HEROKU */
 var port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
+
 
 app.use(express.static('public'));
 app.get("/", function(peticion, respuesta){
@@ -19,12 +21,27 @@ app.get("/", function(peticion, respuesta){
 //    });
 //});
 
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
 io.on("connection", function(socket){
     socket.on("chat message", function(msg){
-        //$('#line').append($('<li>').text(msg));
         console.log("message --> " + msg);
         io.emit('chat message', msg);
-    })
+    });
 });
 
 
