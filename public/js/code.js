fd661763be0c4c2204115e40c65d2e7262ee5cec
Jose Mª R.F <Jose M R.F>
2017-02-23 20:11:49 +0100

    more changes

--- public/js/code.js
+++ public/js/code.js
@@ -1,26 +1,71 @@
-/*  */
 function escapeHtml(text) {
-    return text
+    text += " ";
+    text = text
         .replace(/&/g, "&amp;")
         .replace(/</g, "&lt;")
         .replace(/>/g, "&gt;")
         .replace(/"/g, "&quot;")
         .replace(/'/g, "&#039;");
+    
+    if (text.length > 0){
+        var result = text.match(/\s/g);
+        if (text.length > result.length){
+            return text;
+        }else{return false;}
+    }else{return false;}
+}
+
+function selectNick(){
+    var val = $("#nick").val();
+    var state = $("#st").val();
+    var img = $('input[name="a"]:checked').val();
+    val = escapeHtml(val);
+    state = escapeHtml(state);
+    if (val.length > 0 && state.length > 0 && img.length > 0){
+        $("#myModal").modal('hide');
+        
+        socket.emit('showUser', val, state, img);
+        socket.emit("addUser", val);
+    }
 }
 
 $(function(){
+	$(document).ready(function(){
+		$("#myModal").modal('show');
+	});
     $("input").focus();
 });
 
 
 var socket = io();
-$('form').submit(function(){
-    socket.emit('chat message', $('#text').val());
+$('#chat').submit(function(){
+    socket.emit('chat message', $('#text').val(), "send");
     $('#text').val('');
     return false;
 });
 
-socket.on('chat message', function(msg){
+socket.on("delUser", function(){
+    $('#line').append($('<li class="new">Usuario <font color="red">desconectado</font></li>'));
+});
+
+socket.on("addUser", function(user){
+    $('#line').append($('<li class="new">Usuario [' + user + '] <font color="green">conectado</font></li>'));
+});
+
+socket.on('chat message', function(msg, cls){
     msg = escapeHtml(msg);
-    $('#line').append($('<li> ' + msg + '</li>'));
+    if (msg){
+        $('#line').append($('<li class="' + cls + '"><b>' + socket.nick + "</b>: " + msg + '</li>'));
+        console.log(socket.nick);
+    }
+});
+
+socket.on("showUser", function(text, state, img){
+    text = escapeHtml(text);
+    if (text){
+        var video = ('<p><span class="glyphicon glyphicon-facetime-video"></span></p>');
+        var private = ('<p><span class="glyphicon glyphicon-lock"></span></p> ');
+        var div = ('<hr/><div>State: ' + state + '</div>');
+        $("#user").append($("<li><img class='ava' src='" + img + "'/>" + text + video + private + div + "</li>"));
+    }
 });
--- server/server.js
+++ server/server.js
@@ -15,7 +15,6 @@ function normalizePort(val) {
     }
 
     if (port >= 0) {
-        // port number
         return port;
     }
 
@@ -24,19 +23,36 @@ function normalizePort(val) {
 var port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
-
 app.use(express.static('public'));
 app.get("/", function(peticion, respuesta){
     respuesta.sendFile('index.html');    
 });
 
+var userList = [];
 io.on("connection", function(socket){
-    socket.on("chat message", function(msg){
-        console.log("message --> " + msg);
-        io.emit('chat message', msg);
+    userList.push(socket);
+    socket.on("addUser", function(user){
+        socket.nick = user;
+        io.emit("addUser", user);
+    });
+    socket.on("chat message", function(msg, cls){
+        console.log("Conexion: message --> " + msg);
+        io.emit('chat message', msg, cls);
+    });
+    socket.on("disconnect", function(){
+        io.emit("delUser");
+        
+        var i = userList.indexOf(socket);
+        userList.splice(i, 1);
+        
+        console.log("user online: " + userList.length);
+    });
+    
+    socket.on("showUser", function(text, state, img){
+        io.emit('showUser', text, state, img);
     });
 });
 
 http.listen(port, function(){
-    console.log("listening on port 3000...");
+    console.log("listening on port XXXX...");
 });
